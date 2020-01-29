acbc5c541df2faad686db11f6a3312c876e1bfb2
Rand Farhan <rand.farhan@gmail.com>
2020-01-29 14:51:42 +0100

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDataBase.Models;
+
+namespace MVCDataBase.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/ Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Fury!" };
+            var customer = new List<Customer>
+
+
+            {
+                new Customer { Name ="Customer 1"},
+                new Customer { Name ="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customer = customer
+            };      
+
+            return View(viewModel);
+        }
+    }
+}
